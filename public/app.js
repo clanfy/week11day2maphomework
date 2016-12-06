174f6e569db085e9c50c540a9ef4c85ce134c773
Clacson <cookiellacson@gmail.com>
2016-12-06 20:23:50 +0000

    info window completed

--- public/app.js
+++ public/app.js
@@ -3,10 +3,14 @@ var init = function(){
   var center = {lat: 48.46464113630569, lng: -123.30790758132935};
 
   var mainMap = new MapWrapper(container, center, 16);
-  mainMap.addMarker(center);
 
+  var information = "<b>Felicitas pub</b>, home of the greasiest pub on UVIC campus. Specialising in sticky floors, 80's linoleum decor, student servers that look like the poltergeist-seeing kid from The Sixth Sense, awful cheap beer in jugs and tacky dress-up charity parties. Cookie used to hang out here with her best friends and even won $30, a pint glass and a shoe horn from coming in Second Place in a pub quiz! <i> Fun fact: </i> Cookie has barfed in the bathrooms more times than she can count. " ;
 
+  mainMap.addMarker(center, information);
   mainMap.addClickEvent();
+
+
+
 
 };
 
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -8,24 +8,33 @@ this.googleMap = new google.maps.Map(container,
 
 MapWrapper.prototype = {
 
-  addMarker: function(coords){
+  addMarker: function(coords, information){
     var marker = new google.maps.Marker({
       position: coords, 
-      map: this.googleMap
+      map: this.googleMap,
+      title: 'UVIC (Party Central)'
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+      content: information
+    });
+
+    marker.addListener('click', function(){
+      infowindow.open(this.googleMap, marker);
     });
   },
   //binding .this, in this function to always be the map wrapper
   addClickEvent: function(){
     google.maps.event.addListener(this.googleMap, 'click', function(event){
-      console.log("Yo I got called");
-      console.log(event);
-      console.log(event.latLng.lat());  
-      console.log(event.latLng.lng());
-      // var position = {lat: event.latLng.lat(), lng: event.latLng.lng()};
       var position = event.latLng;
       console.log(this);
       this.addMarker(position);
     }.bind(this));
+  },
+  createInfoWindow: function(){
+    var infowindow = new google.maps.InfoWindow({
+    content: "content string stuff" 
+    });
   }
 
 };
